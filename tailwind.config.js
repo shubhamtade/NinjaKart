131d2cdd66fd6a0f61e92b1baf36254d1e2f9ba0
Shubham Tade <shubham.tade@gmail.com>
2024-12-21 10:40:28 +0530

    HeroScreen Change

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      sm: "380px",
+      sm: "576px",
       // => @media (min-width: 576px) { ... }
 
       md: "960px",
