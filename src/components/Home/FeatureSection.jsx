0f68cbdac4c36a441ad49ff2954adc4803fe0154
Shubham Tade <shubham.tade@gmail.com>
2024-12-22 02:30:18 +0530

    Feature Section Done

--- src/components/Home/FeatureSection.jsx
+++ src/components/Home/FeatureSection.jsx
@@ -9,22 +9,22 @@ const FeatureSection = () => {
     <div className="my-6 md:my-12 mx-[10%] text-center flex flex-wrap justify-evenly gap-5 opacity-60">
       <div>
         <LocalShippingOutlinedIcon fontSize="large" />
-        <h2 className="text-2xl font-bold">Free Shipping</h2>
+        <h2 className="text-xl md:text-2xl font-bold">Free Shipping</h2>
         <p>Order above ₹200</p>
       </div>
       <div>
         <PaymentsOutlinedIcon fontSize="large" />
-        <h2 className="text-2xl font-bold">Money-back</h2>
+        <h2 className="text-xl md:text-2xl font-bold">Money-back</h2>
         <p>30 days guarantee</p>
       </div>
       <div>
         <LockOutlinedIcon fontSize="large" />
-        <h2 className="text-2xl font-bold">Secure Payments</h2>
+        <h2 className="text-xl md:text-2xl font-bold">Secure Payments</h2>
         <p>Secured by Stripe</p>
       </div>
       <div>
         <CallOutlinedIcon fontSize="large" />
-        <h2 className="text-2xl font-bold">24/7 Support</h2>
+        <h2 className="text-xl md:text-2xl font-bold">24/7 Support</h2>
         <p>Phone and Email support</p>
       </div>
     </div>
